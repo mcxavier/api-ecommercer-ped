34ce18de933b7a91288a169b20dbee52d9698c41
Mauricio Xavier <mcxavier2@gmail.com>
2026-09-01 22:37:18 -0300

    Add README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # api-ecommercer-ped
-Api de gestão de pedidos de um e-commerce
+Api de gestão de pedidos de um e-commerce estrutura em arquitetura CRQS e Clean Architecture.
+
+O codigo esta organizado nas camadas: Core, Infraestructure, e Presentation.
+
+Na cama de Core, estão agrupados os codigos de Domain e Application;
+Já na camada Infraestructure, estão os codigos da camada de persistencia
+E por fim, na camada de Presentation, está a Web Api propriamente dita.
+
+Na web api, utilizei o Swagger pra documentação, e escolhi a estrutura de Controllers ao invés das minimals apis, e a raão pra isso é que acho que o projeto fica mais estruturado e organizado
+
+
+
+
--- api-ecommerce-ped.web/Program.cs
+++ api-ecommerce-ped.web/Program.cs
@@ -2,15 +2,9 @@ using api_ecommercer_ped.persistence.Context;
 using api_ecommercer_ped.webapi.Extensions;
 using api_ecommercer_ped.application.Services;
 using api_ecommercer_ped.persistence;
-using api_ecommercer_ped.persistence.Context;
-using api_ecommercer_ped.webapi.Extensions;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>();
@@ -25,11 +19,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 
-//if (app.Environment.IsDevelopment())
-//{
-app.UseSwagger();
-app.UseSwaggerUI();
-//
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
